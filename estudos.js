2d6fe712fee29ef3578d34f1013a46a6fb10ce2a
Alexsouza <96217395+AleSouza7@users.noreply.github.com>
2022-05-03 17:45:47 -0300

    Add files via upload

---
+++ estudos.js
@@ -0,0 +1,77 @@
+/*AULA DO VIDEO 152
+var texto = 'Curso de JavaScript';
+
+var numeroInteiro =-7;
+var numeroFracionado = 11210.75;
+
+var teste = false;
+
+document.write(texto)
+console.log(numersInteiro)
+console.log(numeroFracionado)
+console.log(teste)*/
+
+/*AULA DO VIDEO 153
+
+var nome = prompt('Digite o seu nome');
+var idade =prompt('Digite a sua idade');
+var idade2 =50;
+
+console.log(idade);
+console.log(idade2);
+
+document.write('<h1>Olá ' + nome + ',tudo bem? <br><br><hr><br> Estou vendo aqui que você possui ' + idade +' anos</h1>');
+
+*/
+
+/*aula 155 de javascript
+
+//null ausencia intencional de dados
+var teste = null
+
+//undefined
+var teste2
+
+console.log(teste);
+console.log(teste2);
+*/
+
+//aula 156
+/*
+var teste = null
+
+console.log(teste)
+
+teste = 'Valor modificado com sucesso'
+
+console.log(teste)
+
+teste =127.48
+
+console.log(teste)
+*/
+
+
+//desafio a,b,c invertendo os valores de a e b
+var a =10;
+var b =20;
+var c =null;//variavel auxiliar
+
+document.write('A: '+ a +'<br/>');
+document.write('B: '+ b +'<br/>');
+document.write('C: '+ c +'<br/>');
+
+document.write('<br/><hr/><br/>')
+document.write('<h4>Resultado da inversão dos valores das variáveis A e B</h4><br/>');
+
+var c = a //c passou a ser 10 e a null 
+var a = b //a passou a ser 20 e b null
+var b = c //b passou a ser 10 e c null
+var c =null
+//
+document.write('A: '+ a +'<br/>');
+document.write('B: '+ b +'<br/>');
+document.write('C: '+ c +'<br/>');
+
+
+
